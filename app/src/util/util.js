74a52196af5319111bbc2abd98adfd42be32956a
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-02-17 10:50:24 +0800

    util functions file
    - function for extracting unique values of an array

---
+++ app/src/util/util.js
@@ -0,0 +1,24 @@
+/**
+ *Ensures that all values from a given array are unique
+ * @returns An array containing the unique values
+ * @param {Array} array
+ */
+export const uniqArray = array => {
+  //check if param is array
+  if (!Array.isArray(array)) {
+    return;
+  }
+
+  const s = new Set();
+  const a = [];
+
+  array.forEach(itm => {
+    //check if Set does not have the value, then add them to Set and the array to be returned
+    if (!s.has(itm.path)) {
+      s.add(itm.path);
+      a.push(itm);
+    }
+  });
+
+  return a;
+};
