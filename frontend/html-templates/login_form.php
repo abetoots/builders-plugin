a4dac19a65c04da20f5fcf99a709da3f8bfcbfa2
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-02-17 11:03:45 +0800

    fix class names, add notifications

--- frontend/html-templates/login_form.php
+++ frontend/html-templates/login_form.php
@@ -1,27 +1,29 @@
 <?php if (!defined('ABSPATH')) exit; // Exit if accessed directly 
 ?>
 
-<div id="LoginForm" class="LoginForm">
+<div class="LoginForm">
 
     <!-- Show errors if there are any -->
-    <?php if (count($attributes['errors']) > 0) : ?>
-        <?php foreach ($attributes['errors'] as $error) : ?>
-            <p class="LoginForm__error">
-                <?php echo esc_html($error); ?>
-            </p>
-    <?php endforeach;
-    endif; ?>
+    <div class="LoginForm__notifications">
+        <?php if (count($attributes['errors']) > 0) : ?>
+            <?php foreach ($attributes['errors'] as $error) : ?>
+                <p class="LoginForm__error">
+                    <?php echo esc_html($error); ?>
+                </p>
+        <?php endforeach;
+        endif; ?>
 
-    <!-- Show logged out message if user just logged out -->
-    <?php if ($attributes['logged_out']) : ?>
-        <p class="LoginForm__info -loggedOut">
-            <?php _e('You have signed out. <br> Would you like to sign in again?', 'builders-plugin'); ?>
-        </p>
-    <?php endif; ?>
+        <!-- Show logged out message if user just logged out -->
+        <?php if ($attributes['logged_out']) : ?>
+            <p class="LoginForm__info -loggedOut">
+                <?php _e('You have signed out. <br> Would you like to sign in again?', 'builders-plugin'); ?>
+            </p>
+        <?php endif; ?>
+    </div>
 
     <h2 class="LoginForm__title"><?php _e($attributes['title'], 'builders-plugin') ?></h2>
 
-    <form method="post" action="<?php echo esc_url(wp_login_url()); ?>" autocomplete="on">
+    <form id="login-form" method="post" action="<?php echo esc_url(wp_login_url()); ?>" autocomplete="on">
         <div class="LoginForm__username">
             <label for="user_login"><?php _e('Email or Username', 'builders-plugin'); ?></label>
             <div>
