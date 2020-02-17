de0ade422bbdb2c38bc598a6f16901f9c3e8cf34
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-02-17 10:33:26 +0800

    script for instantiating flatpickr
    - used by WP

--- frontend/js/form-datepicker.js
+++ frontend/js/form-datepicker.js
@@ -1,7 +1,9 @@
 (function($) {
   var DPinstance = $("#birthdate").flatpickr({
     onChange: function(selectedDates, dateStr, instance) {
+      console.log(dateStr);
       $('[data-hidden-val="datepicker"]').val(dateStr);
-    }
+    },
+    dateFormat: "d-m-Y"
   });
 })(jQuery);
