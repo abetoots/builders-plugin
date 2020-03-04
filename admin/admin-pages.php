6333dd1e5497264d53106e680af2803df3102be7
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-04 21:31:31 +0800

    use the plugin prefix constant

--- admin/admin-pages.php
+++ admin/admin-pages.php
@@ -1,6 +1,10 @@
-<?php settings_errors(); ?>
+<?php
+
+use const Builders_Plugin\Constants\PLUGIN_PREFIX;
+
+settings_errors(); ?>
 <form method="post" action="options.php" class='BuildersPlugin__adminForm'>
     <?php settings_fields('builders-plugin-settings'); ?>
-    <?php do_settings_sections('builders_plugin_do_settings_section'); ?>
+    <?php do_settings_sections('' . PLUGIN_PREFIX . '_do_settings_section'); ?>
     <?php submit_button(); ?>
 </form>
