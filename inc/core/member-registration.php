ca55ca63d69210527586e6bc3dd99c5e4a3b91e3
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-04 21:42:39 +0800

    replace plugin prefix
    -form response should now be in array when passing to do_register_member
    - validation now expects an array of data
    -remove query arg 'gend' on success
    - define a base $metas to iterate in
    register_meta and register_graphql_field
    -fix comments, add actions

--- inc/core/member-registration.php
+++ inc/core/member-registration.php
@@ -2,13 +2,21 @@
 
 namespace Builders_Plugin\Inc\Core;
 
+use Builders_Plugin\Inc\Helpers\Validation;
 use WP_Error;
+
+use const Builders_Plugin\Constants\ACTION_REGISTER_GYM_MEMBER;
+use const Builders_Plugin\Constants\FULL_NAME;
+use const Builders_Plugin\Constants\BRANCH;
+use const Builders_Plugin\Constants\GYM_ROLE;
+use const Builders_Plugin\Constants\MEMBERSHIP_DURATION;
+use const Builders_Plugin\Constants\PLUGIN_PREFIX;
+use const Builders_Plugin\Constants\IS_STUDENT;
 
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 class Member_Registration
 {
-
     /**
      * A shortcode for rendering the new user registration form.
      *
@@ -33,11 +41,10 @@ class Member_Registration
         $attributes = shortcode_atts($default_attributes, $user_attributes);
 
         // Retrieve and assign recaptcha key to array
-        $attributes['recaptcha_site_key'] = get_option('builders_plugin_recaptcha_site_key', null);
+        $attributes['recaptcha_site_key'] = get_option('' . PLUGIN_PREFIX . '_recaptcha_site_key', null);
 
         if (isset($_REQUEST['success'])) {
             $attributes['success'] = true;
-            $attributes['gender'] = $_REQUEST['gend'];
         }
 
         if (!get_option('allow_portal_registration')) {
@@ -52,12 +59,12 @@ class Member_Registration
                 $error_codes = explode(',', $_REQUEST['registration-err']);
 
                 foreach ($error_codes as $error_code) {
-                    $attributes['errors'][] = \Builders_Plugin\Inc\Helpers\Registration::instance()->get_error_message($error_code);
+                    $attributes['errors'][] = Validation::instance()->get_error_message($error_code);
                 }
             }
 
             // Rendering of html is done here
-            return \Builders_Plugin\Inc\Helpers\Registration::instance()->get_template_html('reg_form_gym_member', $attributes);
+            return Validation::instance()->get_template_html('reg_form_gym_member', $attributes);
         }
     }
 
@@ -72,8 +79,6 @@ class Member_Registration
      */
     public function handle_form_response()
     {
-
-
         if (!isset($_POST['register_gym_member_nonce'])) {
             wp_die('first');
         }
@@ -86,25 +91,17 @@ class Member_Registration
             return;
         }
 
-
-        $fullname =  $_POST['full_name'];
-        $email =  $_POST['email'];
-        $gender = $_POST['gender'];
-        $birthdate = $_POST['birthdate'];
-        $isStudent = $_POST['is_student'];
-        $branch = $_POST['branch'];
+        $data = array(
+            'username'  => $_POST[FULL_NAME],
+            FULL_NAME    => $_POST[FULL_NAME],
+            IS_STUDENT     => $_POST[IS_STUDENT],
+            BRANCH      => $_POST[BRANCH],
+            MEMBERSHIP_DURATION => $_POST[MEMBERSHIP_DURATION]
+        );
 
         //Handles validation and redirect
-        $this->do_register_member(
-            $fullname,
-            $email,
-            $gender,
-            $birthdate,
-            $isStudent,
-            $branch
-        );
+        $this->do_register_member($data);
     }
-
 
     /**
      * Handles the registration of a new user.
@@ -115,7 +112,7 @@ class Member_Registration
      * 
      * @uses $this->validate_and_register_new_user()
      */
-    public function do_register_member($fullname, $email, $gender, $birthdate, $isStudent, $branch)
+    public function do_register_member($data)
     {
         $redirect_url = home_url('registration');
         $errors = new WP_Error();
@@ -123,20 +120,15 @@ class Member_Registration
         if (!get_option('allow_portal_registration')) {
             // Portal registration disabled, display error
             $redirect_url = add_query_arg('registration-err', 'disabled', $redirect_url);
-        } elseif (get_option('builders_plugin_recaptcha_site_key') && get_option('builders_plugin_recaptcha_secret_key')) {
-            if (!\Builders_Plugin\Inc\Helpers\Registration::instance()->verify_recaptcha()) {
+        } elseif (get_option('' . PLUGIN_PREFIX . '_recaptcha_site_key') && get_option('' . PLUGIN_PREFIX . '_recaptcha_secret_key')) {
+            if (!Validation::instance()->verify_recaptcha()) {
                 //Recaptcha check failed, display error
                 $redirect_url = add_query_arg('registration-err', 'captcha', $redirect_url);
             }
         } else {
             //either an error or the user id
-            $result = \Builders_Plugin\Inc\Helpers\Registration::instance()->validate_and_register_new_user(
-                $fullname,
-                $email,
-                $gender,
-                $birthdate,
-                $isStudent,
-                $branch,
+            $result = Validation::instance()->validate_and_register_new_user(
+                $data,
                 'gym_member'
             );
 
@@ -147,13 +139,13 @@ class Member_Registration
             } else {
                 // Success, redirect to login page.
                 $redirect_url = home_url('registration');
-                $redirect_url = add_query_arg('success', $email, $redirect_url);
-                $redirect_url = add_query_arg('gend', $gender, $redirect_url);
+                $redirect_url = add_query_arg('success', $data[FULL_NAME], $redirect_url);
             }
         }
         wp_redirect($redirect_url);
         exit;
     }
+
 
     /**
      * An action function used to include the reCAPTCHA JavaScript file
@@ -162,6 +154,73 @@ class Member_Registration
     public function add_captcha_js_to_footer()
     {
         echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+    }
+
+    public static $metas = [
+        FULL_NAME => [
+            'type'  => 'string',
+            'rest'  => false,
+            'obj_type'  => 'User',
+        ],
+        IS_STUDENT  => [
+            'type'  => 'number',
+            'rest'  => false,
+            'obj_type'  => 'User',
+        ],
+        GYM_ROLE  => [
+            'type'  => 'string',
+            'rest'  => false,
+            'obj_type'  => 'User'
+        ],
+        BRANCH  => [
+            'type'  => 'string',
+            'rest'  => false,
+            'obj_type'  => 'User'
+        ],
+        MEMBERSHIP_DURATION => [
+            'type'  => 'string',
+            'rest'  => false,
+            'obj_type'  => 'User'
+        ]
+    ];
+
+    /**
+     * Define user metas and show in REST API
+     * see https://docs.wpgraphql.com/getting-started/custom-fields-and-meta/
+     */
+
+    private function register_user_metas()
+    {
+        foreach (self::$metas as $key => $val) {
+            register_meta($val['obj_type'], $key, array(
+                "type" => $val['type'],
+                "show_in_rest" => $val['rest']
+            ));
+        }
+    }
+
+    public function register_user_metas_in_wpgraphql()
+    {
+        foreach (self::$metas as $key => $val) {
+            register_graphql_field($val['obj_type'], $key, array(
+                //The schema only has 'Int' type, everything else convert to uppercase
+                'type' => $val['type'] === 'number' ? 'Int' : strtoupper($val['type']),
+                'resolve' => function ($obj, $dunno, $app_context, $resolve_info) {
+                    if ($resolve_info->fieldName === GYM_ROLE) {
+                        $role = get_userdata($obj->userId)->roles[0];
+                        if ($role === 'gym_member' || $role === 'gym_trainer' || $role === 'gym_admin' || $role === 'administrator') {
+                            return $role;
+                        }
+                    } elseif ($resolve_info->returnType->name === 'Int') {
+                        $return = get_user_meta($obj->userId, $resolve_info->fieldName, true) || 0;
+                        return $return;
+                    } else {
+                        $return = get_user_meta($obj->userId, $resolve_info->fieldName, true);
+                        return $return;
+                    }
+                }
+            ));
+        }
     }
 
     /**
@@ -195,8 +254,6 @@ class Member_Registration
     /**
      *  Plugin class constructor
      *
-     * Register plugin action hooks and filters
-     *
      * @since 1.0.0
      * @access public
      */
@@ -213,7 +270,9 @@ class Member_Registration
      */
     public function init()
     {
-
+        //Register user metas
+        $this->register_user_metas();
+        add_action('graphql_register_types', array($this, 'register_user_metas_in_wpgraphql'));
         //Registration
         add_shortcode('builders-reg-form-member', array($this, 'render_registration_form'));
 
@@ -221,7 +280,7 @@ class Member_Registration
         add_action('wp_print_footer_scripts', array($this, 'add_captcha_js_to_footer'));
 
         // Handle form response
-        add_action('admin_post_nopriv_builders_do_action_register_gym_member', array($this, 'handle_form_response'));
+        add_action('admin_post_nopriv_' . ACTION_REGISTER_GYM_MEMBER, array($this, 'handle_form_response'));
     }
 }
 Member_Registration::instance();
