618a97ee2db20d77958a055558b785e3d157c583
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-26 16:48:43 +0800

    re arrange

--- inc/core/constants.php
+++ inc/core/constants.php
@@ -2,19 +2,24 @@
 
 namespace Builders_Plugin\Constants;
 
-/**
- * Gym user types keys
- */
+//!DO NOT MODIFY
+//WP GraphQl
+const JWT_AUTH_EXPIRATION = "jwtAuthExpiration";
+const REFRESH_TOKEN = "refreshToken";
+const AUTH_TOKEN = "authToken";
+
+//Plugin
+const PLUGIN_PREFIX = "builders_plugin";
+const VALIDMEMBERSHIPFORMAT = "Ymd";
+const ACTION_REGISTER_GYM_MEMBER =
+"builders_do_action_register_gym_member";
+
+//Roles
 const GYM_MEMBER = "gym_member";
 const GYM_TRAINER = "gym_trainer";
 const GYM_ADMIN = "gym_admin";
 
-/**
- * Gym member meta keys / WPGraphQl keys
- */
-const JWT_AUTH_EXPIRATION = "jwtAuthExpiration";
-const REFRESH_TOKEN = "refreshToken";
-const AUTH_TOKEN = "authToken";
+//User Database keys
 const FULL_NAME = "full_name";
 const MEMBERSHIP_DURATION = "membership_duration";
 const IS_STUDENT = "is_student";
@@ -35,13 +40,3 @@ const HALF_YEAR = '180 days';
 const ONE_YEAR = '1 year';
 
 const GYM_USER_ALLOWED_EDITABLE_FIELDS = [FULL_NAME, MEMBERSHIP_DURATION];
-
-/**
- * Shared
- */
-const PLUGIN_PREFIX = "builders_plugin";
-const ACTION_REGISTER_GYM_MEMBER =
-"builders_do_action_register_gym_member";
-const ACTION_AJAX_REGISTER_GYM_MEMBER =
-"builders_do_action_register_gym_member_app";
-const VALIDDATEFORMAT = "Ymd";
